96503b84dc892bc804bb77145c0477686f1e8dfb
blazingh <hadibaalbaki2@gmail.com>
2022-07-16 19:52:12 +0300

    you died

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import SignUp from "./SignUp";
 function App() {
 	return (
 		<div className="flex flex-wrap items-center justify-center">
-			<div className="w-full h-full max-w-md mx-1 lg:mx-5 my-5">
+			<div className="w-full h-full max-w-md mx-2 lg:mx-5 my-5">
 				<Profile />
 			</div>
-			<div className="w-full h-full max-w-md mx-1 lg:mx-5 my-5">
+			<div className="w-full h-full max-w-md mx-2 lg:mx-5 my-5">
 				<SignIn />
 			</div>
-			<div className="w-full h-full max-w-md mx-1 lg:mx-5 my-5">
+			<div className="w-full h-full max-w-md mx-2 lg:mx-5 my-5">
 				<SignUp />
 			</div>
 		</div>
